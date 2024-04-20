09bb0b555c9b1e21682ce2a07b67a7648e78c5b1
titan <titanioy98@gmail.com>
2024-04-20 10:41:58 +0700

    init commit

--- soal-1.js
+++ soal-1.js
@@ -9,7 +9,6 @@ Tolong buat satu array / list dari 1 sampai 100. Print semua angka ini dalam uru
 
 Output:
 
-❯ node soal-1.js
 Bar Foo 98  Foo Bar 94 Foo 92 91 FooBar  88 Foo 86 Bar Foo  82 Foo Bar  Foo 77 76 FooBar 74  Foo  Bar Foo 68  Foo Bar 64 Foo 62  FooBar  58 Foo 56 Bar Foo  52 Foo Bar 49 Foo  46 FooBar 44  Foo  Bar Foo 38  Foo Bar 34 Foo 32  FooBar  28 Foo 26 Bar Foo  22 Foo Bar  Foo  16 FooBar 14  Foo  Bar Foo 8  Foo  4   1
 
  */
